5f21e6a8b8ae42794f519c1109fec9faef30824c
aerocanlas <princeaero.canlas.cics@ust.edu.ph>
2024-02-25 21:53:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+import q from "./data/data.json";
+import { ref, watch } from "vue";
+import Cards from './components/Cards.vue'
+
+const icons = ref(q);
+const search = ref("");
+const showModal = ref(false);
+
+watch(search, () => {
+  icons.value = q.filter(icon => icon.name.toLowerCase().includes(search.value.toLowerCase()))
+})
+</script>
+
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Icons Repository</h1>
+      <input type="text" v-model.trim="search" placeholder="Search icons..." />
+    </header>
+    
+    <div class="options-container">
+      <Cards v-for="icon in icons" :key="icon.id" :icon="icon"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 10px;
+}
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import { defineProps } from 'vue';
+
+const { icon } = defineProps(['icon'])
+
+</script>
+
+<template>
+      <div class="card" >
+        <img :src='icon.img' />
+        <div class="card-text">
+          <h2> {{icon.name}}</h2>
+        </div>
+      </div>
+
+</template>
+
+<style scoped>
+
+.card {
+  width: calc(25% - 20px);
+  margin-bottom: 20px;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  margin-top: 20px;
+}
+.card img {
+  width: 100%;
+  height: auto;
+}
+.card-text {
+  padding: 0 5px;
+}
+.card-text h2 {
+  font-weight: bold;
+  text-align: center;
+  font-size: 16px;
+}
+
+</style>
